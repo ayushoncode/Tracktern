5b249a69c821afefb0fe4326e51957452f133232
ayushoncode <ayush916282580@gmail.com>
2026-04-11 23:37:22 +0530

    feat: fix favicon to show purple T

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon.ico' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
     ],
     apple: '/apple-touch-icon.png',
   },
